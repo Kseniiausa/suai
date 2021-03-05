c1a6532c884b462a0bdb409ec3814e9744f6ce97
Kseniia <ksusha.7_7_7@mal.ru>
2021-03-05 19:18:38 +0300

    my homework was added to the project

--- homework/main.cpp
+++ homework/main.cpp
@@ -6,9 +6,38 @@
 //
 
 #include <iostream>
-
+#include <iomanip>
 int main(int argc, const char * argv[]) {
+
+/*
+ 7) Напишите программу пересчета величины временного интервала, заданного в минутах, в величину, выраженную в часах и минутах.
+ */
+
+
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int minutes;
+    std::cout << "Введите колличество минут"<< std::endl;
+    std::cin >> minutes;
+    int hours = minutes / 60;
+    int minute = minutes - hours*60;
+    std::cout << hours << " ч. "<< minute << " мин." << std::endl;
+   
+    /*
+    7) Напишите программу пересчета величины временного интервала, заданного в минутах, в величину, выраженную в часах и минутах.
+    */
+    
+    float price;
+    
+    std::cout << "Введите цену 1 кг яблок:" << std::endl;
+    std::cin >> price;
+    
+    std::cout << "Вес \tСтоимость" << std::endl;
+    std::cout << "(г) \t(руб.)" << std::endl;
+    
+    for (int i = 1; i<=10; i++) {
+        std::cout << i*100 <<"\t\t" <<std::fixed<<std::setprecision(2) << i*price*0.10 << std::endl;
+        
+    }
+    
     return 0;
 }
